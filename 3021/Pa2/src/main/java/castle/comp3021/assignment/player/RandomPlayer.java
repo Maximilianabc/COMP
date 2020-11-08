595521dd747a197813f1d8b9edc542fa242cbe54
Maximilian <leunghomanmax@rocketmail.com>
2020-11-09 00:49:45 +0800

    123123123

---
+++ 3021/Pa2/src/main/java/castle/comp3021/assignment/player/RandomPlayer.java
@@ -0,0 +1,25 @@
+package castle.comp3021.assignment.player;
+
+import castle.comp3021.assignment.protocol.*;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.*;
+
+/**
+ * A computer player that makes a move randomly.
+ */
+public class RandomPlayer extends Player {
+    public RandomPlayer(String name, Color color) {
+        super(name, color);
+    }
+
+    public RandomPlayer(String name) {
+        this(name, Color.BLUE);
+    }
+
+    @Override
+    public @NotNull Move nextMove(Game game, Move[] availableMoves) {
+        int index = new Random().nextInt(availableMoves.length);
+        return availableMoves[index];
+    }
+}
---
+++ 3111/COMP3111-Fall2020-T04/README.md
@@ -0,0 +1,17 @@
+# Team T-04
+> https://github.com/tcm9439/COMP3111-Fall2020-T04
+
+## 1.
+> email: hmmleung | github: Maximilianabc | dev branch: hmmleung_dev
+
+> Task 1 & Task 4
+
+## 2.
+> email: kyhuiaf| github: kyfrankie| dev branch: frankie_dev
+
+> Task 2 & Task 5
+
+## 3. Tse Ching, Maisy
+> email: ctse | github: tcm9439 | dev branch: ctse_dev
+
+> Task 3 & Task 6
